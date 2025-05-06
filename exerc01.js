66fd02d6d769c88d6328ae631338f68736efd126
Luis_Filipe_Varela <luis5rela@outlook.com>
2025-05-06 16:04:02 -0300

    exercicio numero 1 concluido

---
+++ exerc01.js
@@ -0,0 +1,7 @@
+// 1. Crie uma função que receba o nome de uma pessoa como argumento e retorne uma saudação personalizada. Em seguida, chame a função e exiba a saudação no console.
+
+function name (name){
+    return `Eu me chamo ${name}`;
+}
+
+console.log(name("Luis"))
