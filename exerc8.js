87e0ea6084f0792c1ac4e9537237da7a47d0662a
Luis_Filipe_Varela <luis5rela@outlook.com>
2025-05-09 13:41:01 -0300

    exercicio 8 concluído

---
+++ exerc8.js
@@ -0,0 +1,10 @@
+// valor impresso no console: "Usuário logado"
+
+auteticado =true;
+
+switch(auteticado){
+    case true:
+        console.log('Usuário logado');
+    case false:
+        console.log('Usuário não autenticado')
+}
