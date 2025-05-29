0df14d73f4831253a59208b807aeeda7c497bc73
luisFilipeVarela <luis5rela@gmail.com>
2025-05-29 00:12:28 -0300

    Exercicio 15

---
+++ exerc15.js
@@ -0,0 +1,15 @@
+/* Exercício 15: Função de Verificação de Número Par - Crie uma função ehPar(numero) que retorne true se o número for par e false se for ímpar.*/
+
+function ehPar(numero){
+    return numero % 2 ===0;    
+}
+
+console.log(ehPar(4))
+
+console.log(ehPar(7))
+
+console.log(ehPar(-9))
+
+console.log(ehPar(-8))
+
+
