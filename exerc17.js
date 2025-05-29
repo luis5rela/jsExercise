d3a820d43a3cafcaf66a14329ee980589969c395
luisFilipeVarela <153376728+luis5rela@users.noreply.github.com>
2025-05-29 00:45:31 -0300

    exercicios de contagem de caracteres, texto.length

---
+++ exerc17.js
@@ -0,0 +1,15 @@
+/** Exercício 17: Função de Contagem de Caracteres - Crie uma função contarCaracteres(texto) que retorne o número de caracteres em uma string.*/
+
+function contarCaracteres(texto){
+    return texto.length;
+}
+
+console.log(contarCaracteres("JavaScript"))
+
+console.log(contarCaracteres("LuisFilipe"))
+
+console.log(contarCaracteres(" "))
+
+console.log(contarCaracteres(""))
+
+console.log(contarCaracteres("Olá, mundo"))
