b61d21073575f5b9e3ee9240698fa471966bd15a
luisFilipeVarela <153376728+luis5rela@users.noreply.github.com>
2025-05-29 00:38:18 -0300

    exercicios de conversão temperatura celcius para fahrenheit

---
+++ exerc16.js
@@ -0,0 +1,9 @@
+/*Exercício 16: Função de Conversão de Temperatura - Crie uma função celsiusParaFahrenheit(celsius) que converta graus Celsius para Fahrenheit.
+Fórmula: (°C × 9/5) + 32. */
+
+function celsiusParaFahrenheit (celcius){
+
+    return (celcius * 9/5) + 32;
+}
+
+console.log(celsiusParaFahrenheit(25))
